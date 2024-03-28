6be5f82de45e5ddab07046a15fa5e5355b7fee0e
xsf0105 <allan91coder@gmail.com>
2024-03-28 12:58:01 +0000

    Deploying to main from @ hellof2e/quark-core-docs@d4124f68d5eb67c8c423123eecdc3f27c477fc5a 🚀

---
+++ assets/unsafaHTML.en-US.50afae76.js
@@ -0,0 +1,6 @@
+import{d as a,c as n,b as t,o as e}from"./vue.0ac9755c.js";const l={class:"markdown-body"},p=t(`<h2>Unsafe HTML</h2><p>Rendering unsafe HTML structures.</p><pre><code class="language-jsx"><span class="hljs-class"><span class="hljs-keyword">class</span> <span class="hljs-title">MyComponent</span> <span class="hljs-keyword">extends</span> <span class="hljs-title">QuarkElement</span> </span>{
+  <span class="hljs-function"><span class="hljs-title">render</span>(<span class="hljs-params"></span>)</span> {
+    <span class="hljs-keyword">return</span> <span class="xml"><span class="hljs-tag">&lt;<span class="hljs-name">div</span> <span class="hljs-attr">dangerouslySetInnerHTML</span>=<span class="hljs-string">{{__html:</span> \`<span class="hljs-attr">lorem</span> &lt;<span class="hljs-attr">b</span> <span class="hljs-attr">onmouseover</span>=<span class="hljs-string">&quot;alert(&#39;mouseover&#39;);&quot;</span>&gt;</span>ipsum<span class="hljs-tag">&lt;/<span class="hljs-name">b</span>&gt;</span>\`}}  /&gt;</span>
+  }
+}
+</code></pre>`,3),c=[p],j={},m="",i=a({__name:"unsafaHTML.en-US",setup(r,{expose:s}){return s({frontmatter:{},excerpt:void 0}),(o,h)=>(e(),n("div",l,c))}});export{i as default,m as excerpt,j as frontmatter};
